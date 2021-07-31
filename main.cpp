c909e6cfee53f65e510c5d63f7573649fafbf273
aleckarmen <73550854+aleckarmen@users.noreply.github.com>
2021-07-31 13:54:22 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 
 void trim(string &str);
+vector<string> SplitString(string s);
 
 int main() {
 
@@ -19,8 +20,8 @@ int main() {
 
 
 
-    fstream open("Movies.txt", ios::out | ios::app);
-    fstream file("Movies.txt");//change filepath
+    fstream open("NewMovieList.csv", ios::out | ios::app);
+    fstream file("NewMovieList.csv");//filepath from local files
 
 
     vector<movie> movieCollection;
@@ -38,48 +39,69 @@ int main() {
 
         while(getline(file, line))
         {
-            cout <<"Line #: " << count << endl;
+            //cout <<"Line #: " << count << endl;
             istringstream s(line);
 
             string title;
             getline(s, title,',');
+            //cout << "Movie title(s) :" <<title <<"_" << endl;
 
             string stringYear;
             getline(s,stringYear,',');
-            cout << "String year:" <<stringYear <<"_" << endl;
+            //cout << "String year:" <<stringYear <<"_" << endl;
             int year = stoi(stringYear);
 
             string date;
             getline(s,date,',');
 
-            string genre;
-            vector<string> genres;
-            while (!isdigit(stream.peek()))
-            {
-                getline(s,genre,',');  //HERE WE NEED TO FIGURE OUT COMMA IN STRING PREVENTING OUR DELIMITER
-                
-                if (genre[0] == '\"')
-                    genre.erase(genre[0]);
-                if (genre[genre.size() - 1] == '\"')
-                    genre.erase(genre[genre.size() - 1]);
 
-                genres.push_back(genre);
+            /*
+            int quoteCount = 0;
+            vector<string> genreVect;
+            while (quoteCount <2)
+            {
+
+                string genre;
+                getline(s,genre,',');//HERE WE NEED TO FIGURE OUT COMMA IN STRING PREVENTING OUR DELIMITER
+                if(genre.find('"') != string::npos) //if theres a quote in this genre pulled
+                {
+                    quoteCount++;
+                    //trim the quote out
+                }
+                cout << "genre: "<<genre << endl;
+                genreVect.push_back(genre);
+
+
+
+
             }
-            cout << "genre(s): ";
-            for (auto i : genres)
-                cout << i << " ";
+*/
+
+            vector<string> genreVect;
+
+            string genres;
+            getline(s,genres,',');
+
+
+            genreVect = SplitString(genres);
+
+
+            //cout << "genres: " <<genre << endl;
+
+
+
 
             string durationString;
             getline(s,durationString,',');
             trim(durationString);
-            cout << "durationString:" << durationString << "_" << endl;
+            //cout << "durationString:" << durationString << "_" << endl;
             int duration = stoi(durationString);
 
             string country;
             getline(s,country,',');
 
 
-            movie currentMovie(title,genres,date,country,duration,year);
+            movie currentMovie = movie(title,genres,date,country,duration,year, genreVect);
             count++;
 
 
@@ -95,13 +117,45 @@ int main() {
 
     cout << "Count: " << count << endl;
 
-    cout << "First movie in list:" << movieCollection[0].getTitle() << endl;
+    cout << movieCollection[1].getGenreVect().size() << " is the size" << endl;
+    cout << "First movie genre in list:" << movieCollection[1].getGenreVect()[4] << endl;
+    for (int i = 0; i < movieCollection[3].getGenreVect().size(); i++)
+    {
+        cout << movieCollection[3].getGenreVect()[i] << endl;
+    }
+    cout << "First movie genre list:" << movieCollection[1].getGenreVect()[1] << endl;
 
 
 
-    cout << "Last movie in list:" << movieCollection[movieCollection.size()-1].getTitle() << endl;
+    cout << "Last movie in list:" << movieCollection[movieCollection.size()-1].getGenre() << endl;
     return 0;
 }
+
+
+
+vector<string> SplitString(string s)
+{
+    vector<string> v;
+    string temp = "";
+    for(int i=0;i<s.length();++i){
+
+        if(s[i]==' ')
+        {
+            v.push_back(temp);
+            temp = "";
+            i++;
+        }
+        else
+            {
+            temp.push_back(s[i]);//temp is item
+        }
+
+    }
+    v.push_back(temp); //push item
+
+    return v;
+}
+
 
 void trim(string &str)
 {
@@ -111,5 +165,3 @@ void trim(string &str)
         str = str.substr( startpos );
     }
 }
-
-
